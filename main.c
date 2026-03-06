f25c90b60946a797145a2e367cd3e6d2fed59c0f
RumeshDilhara <rumeshdilhara0511@gmail.com>
2026-03-06 14:21:14 +0530

    Added file handling to save and load expenses

--- main.c
+++ main.c
@@ -7,12 +7,24 @@ struct Expense {
 };
 
 int main() {
+
+    FILE *fp;
     struct Expense myExpenses[100];
     int count = 0;
     int choice = 1;
     float total = 0;
 
     printf("    Daily Expense Tracker    \n");
+
+    fp = fopen("expenses.txt", "r");
+
+    if (fp != NULL) {
+    while (fscanf(fp, "%s %f", myExpenses[count].name, &myExpenses[count].amount) != EOF) {
+        total += myExpenses[count].amount;
+        count++;
+    }
+    fclose(fp);
+}
 
     while (choice != 0) {
         printf("\n1. Add Expense\n0. Exit & Show Total\nChoice: ");
@@ -24,6 +36,12 @@ int main() {
                 scanf(" %[^\n]", myExpenses[count].name);
                 printf("Enter amount: ");
                 scanf("%f", &myExpenses[count].amount);
+                
+                FILE *fp;
+                fp = fopen("expenses.txt", "a");
+                fprintf(fp, "%s %.2f\n", myExpenses[count].name, myExpenses[count].amount);
+                fclose(fp);
+
                 total += myExpenses[count].amount;
                 count++;
             } else {
@@ -38,5 +56,16 @@ int main() {
     }
     printf("Total Expense: %.2f\n", total);
 
+
     return 0;
+
+
+
+
+
+
+
+
+
+    
 }
